d99b487525a0e34511cd9c3eb87485cdf00fd946
Mathieu <92155925+MathieuDubart@users.noreply.github.com>
2024-07-18 15:23:43 +0200

    adding LocalNotifications

---
+++ LocalNotifications/LocalNotifications/LocalNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationsApp.swift
+//  LocalNotifications
+//
+//  Created by Mathieu Dubart on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
